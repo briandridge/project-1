197e7fd218b4de1af1b0188bc08dc97938ced7d4
briandridge <briandridge@gmail.com>
2016-11-16 11:38:10 -0700

    fixes key event handlers, working on blaster

--- js/app.js
+++ js/app.js
@@ -1,22 +1,42 @@
-console.log("linked");
-
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
-var dy = -4
+var dy = -4;
 var blasterRadius = 10;
 var shipWidth = 50;
 var shipHeight = 50;
 var shipX = (canvas.width-shipWidth)/2;
+var shipY = canvas.height-30;
 var rightPressed = false;
 var leftPressed = false;
+var spacePressed = false;
 
-// no paddle boucing, so need an origin... when key listener or click call function???
+document.addEventListener("keydown", function(event) {
+	if (event.keyCode == 39) {
+		rightPressed = true;
+	}	
+	else if (event.keyCode == 37) {
+		leftPressed = true;
+	}
+});
+
+document.addEventListener("keyup", function(event) {
+	if (event.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if (event.keyCode == 37) {
+		leftPressed = false;
+	}
+	else if (event.keyCode == 32) {
+		spacePressed = true;
+	}
+});
+
 var drawBlaster = function() {
 	ctx.beginPath();
-	ctx.arc(x, y, blasterRadius, 0, Math.PI*2);
+	ctx.arc(shipX, y, blasterRadius, 0, Math.PI*2);
 	ctx.fillStyle = "red";
 	ctx.fill();
 	ctx.closePath();
@@ -32,30 +52,31 @@ var drawShip = function() {
 
 var draw = function() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawBlaster();
 	drawShip();
-	y += dy;
+	if (rightPressed && shipX < canvas.width-shipWidth) {
+		shipX += 3;
+	}
+	else if (leftPressed && shipX > 0) {
+		shipX -= 3;
+	}
+	// if (spacePressed) {
+	// 	drawBlaster();
+	// }
 };
 
-setInterval(draw, 10);
-
-document.addEventListener("keydown", keyDownListener, false);
-document.addEventListener("keyup", keyUpListener, false);
-
-var keyDownListener = function(page) {
-	if (page.keyCode == 39) {
-		rightPressed = true;
-	}	
-	else if (page.keyCode == 38) {
-		leftPressed = true;
+var fireBlaster = function(){
+	if (spacePressed) {
+		drawBlaster();
+		y += dy;
+		console.log("fireBlaster");
+		console.log(y);
 	}
 };
 
-var keyUpListener = function(page) {
-	if (page.keyCode == 39) {
-		rightPressed = false;
-	}
-	else if (page.keyCode == 38) {
-		leftPressed = false;
-	}
-};
+
+window.setInterval(function(){
+	draw();
+	fireBlaster();
+	}, 10);
+
+
